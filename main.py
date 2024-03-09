abed897c9dff3d1f5798c48b61b2215651b496fb
Shubhadeep Saha <shubhadeepsaha72@gmail.com>
2024-03-10 01:55:58 +0530

    asd

--- main.py
+++ main.py
@@ -44,23 +44,36 @@ def run_simulation():
     ag = figure.switch_backends(FigureCanvasAgg)
     ag.draw()
     A = np.asarray(ag.buffer_rgba())
-    img=ImageTk.PhotoImage(Image.fromarray(A))
-    canvas.create_image(10,10,anchor=tk.NW, image=img)
+    img=Image.fromarray(A)
+    canvas_width = canvas.winfo_width()
+    canvas_height = canvas.winfo_height()
+    ratio = min(canvas_width / img.width, canvas_height / img.height)
+    new_width = int(img.width * ratio)
+    new_height = int(img.height * ratio)
+    new_image = img.resize((new_width, new_height), Image.ANTIALIAS)
+    new_photo = ImageTk.PhotoImage(new_image)
+    canvas.create_image(0,0,anchor=tk.NW, image=new_photo)
+    canvas.image = new_photo
+
+    # img = Image.fromarray(A)
+    # img.show()
 
 # Create the main window
 root = tk.Tk()
-root.geometry("750x250")
+root.geometry("600x640")
 root.title("Simulator Model")
 
 # Create frames for layout
 #map_frame = tk.Frame(root, width=400, height=200, bd=1, relief=tk.SUNKEN)
-graph_frame = tk.Frame(root, width=400, height=200, bd=1, relief=tk.SUNKEN)
+graph_frame = tk.Frame(root, width=600, height=600, bd=1, relief=tk.SUNKEN)
 button_frame = tk.Frame(root)
 
 # Place the frames in the window
 #map_frame.grid(row=0, column=0, rowspan=2, padx=5, pady=5)
-graph_frame.grid(row=0, column=1, rowspan=2, padx=5, pady=5)
-button_frame.grid(row=2, columnspan=2, padx=5, pady=5)
+# graph_frame.grid(row=0, column=1, rowspan=2, padx=5, pady=5)
+# button_frame.grid(row=2, columnspan=2, padx=5, pady=5)
+graph_frame.pack(side="top", padx=5, pady=5)
+button_frame.pack(side="bottom", fill="x", padx=5, pady=5)
 
 # Add map image (replace with your image path)
 #map_image = tk.PhotoImage(file="D:/final yr project/Our work/Code/maharashtra_map.png")
@@ -68,8 +81,8 @@ button_frame.grid(row=2, columnspan=2, padx=5, pady=5)
 #map_label.pack()
 
 # Create a canvas for the graph
-canvas = tk.Canvas(root, width=600, height=400)
-canvas.grid()
+canvas = tk.Canvas(graph_frame,width=600, height=600)
+canvas.pack()
 
 # Create the run simulation button
 run_button = tk.Button(button_frame, text="Run Simulation", command=run_simulation)
